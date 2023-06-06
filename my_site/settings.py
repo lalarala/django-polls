253547ee4e2f7cefaeca18b6b480d93a2fd522d7
lARA mOREIRA <lara.mes2007@gmail.com>
2023-06-06 10:19:56 -0300

    resumo

--- my_site/settings.py
+++ my_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 
 
-  DATABASES = {
+DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.mysql',
            'NAME': "pollsdb",
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+   
