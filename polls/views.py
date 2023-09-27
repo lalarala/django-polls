2d1988222e212815197fcb6f8f9f0498fc62b330
lalarala <lara.mes2007@gmail.com>
2023-09-27 09:37:14 -0300

    ee

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,9 @@ from .models import Question
 
 from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 
